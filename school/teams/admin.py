411d1c4013a18d7ad729d3ca217898b636b5fea5
Andrzej Derela <andrzejderela@gmail.com>
2018-11-25 11:57:32 +0100

    Add admin

--- school/teams/admin.py
+++ school/teams/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CourseType, Group, Student
 
-# Register your models here.
+@admin.register(CourseType)
+class CourseTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GruopAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
